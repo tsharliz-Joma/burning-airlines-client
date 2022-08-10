1f6e58d19ff8b70d46cae23c08521540cf27eb96
Charles Joma <almprincecharles@yahoo.com>
2022-08-10 16:32:02 +1000

    Some changes

--- src/components/AvailableSeats/AvailableSeats.js
+++ src/components/AvailableSeats/AvailableSeats.js
@@ -2,21 +2,20 @@ import React, { Component } from "react"
 import axios from 'axios'
 import './AvailableSeats.css'
 
-const railsBaseUrl = 'https://localhost:3000'
+const railsBaseUrl = 'https://localhost:3000/flights.json'
 
 class AvailableSeats extends Component {
     contructor(){
         super();
 
         this.state = {
-            seatsTaken: [],
-            seatsAvailable: [], 
+            seats: [],
         }
     }
 
     componentDidMount(){
         const fetchSeats = () => {
-            axios({/*URL HERE*/}).then((response) => {
+            axios({railsBaseUrl}).then((response) => {
                 this.setState({seats: response.data.reservations })
             })
             setTimeout(fetchSeats, 2000)
@@ -26,7 +25,7 @@ class AvailableSeats extends Component {
     }
 
     saveSeat(content){
-        axios.post({/*URL*/}, { content: content }).then((response) =>{
+        axios.post({railsBaseUrl}, { content: content }).then((response) =>{
             this.setState({seats: [...this.state.seats, response.data]})
         })
     }
@@ -35,23 +34,18 @@ class AvailableSeats extends Component {
     render(){
         return(
             <div> 
-            {/* Another option is mapping through every seat that we have an output the available seats*/}
-            <h1>Available Seats</h1>
-                <Seats />
+                {/* Another option is mapping through every seat that we have an output the available seats*/}
+                <h1>Available Seats</h1>
+                <div class="seat-container">
+                    <div class="seat-row">
+                        <div class="seat"> { this.props.seats.map((seat) => <p key={seat.id} className="seat"> {seat.content} {/*Which should be the availability */} </p> )} </div> {/* Within these two divs will be a display with available or Booked/the user name that it belongs to */} {/* Make these two divs inline block so they display left and right of eachother */}
+                    </div>
+                </div>
             </div>
         )
     }
 }
 
-const Seats = (props) => {
-    return (
-        <div class="seat-container">
-            <div class="seat-row">
-                <div class="seat"> { props.seats.map((seat) => <p key={seat.id} className="seat"> {seat.content} {/*Which should be the availability */} </p> )} </div> {/* Within these two divs will be a display with available or Booked/the user name that it belongs to */} {/* Make these two divs inline block so they display left and right of eachother */}
-            </div>
-        </div>
-    )
-}
 
 export default AvailableSeats 
 
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,26 +1,39 @@
 // for the user to search up a flight
 import React, { Component } from 'react';
-import Flights from '../Flight/Flights';
+import axios from 'axios';
+
 
 
 class SearchBar extends Component {
     constructor(props){
         super(props);
+
         this.state = {
             origin: '',
             destination: '',
+            flights: []
         }
 
         this._handleOrigin = this._handleOrigin.bind(this)
         this._handleDestination = this._handleDestination(this)
     }
 
+    _fetchFlights() { // Fetch the flights 
+        const res = axios(  + `/${this.state.origin}` + `/${this.state.destination}`).then((response) => {
+            this.setState({flights: [...this.state.flights, res.data]})
+        })
+    }
+
     // So im thinking that we will need to render flight options from the backend ?
     _renderFlightOptions(){
         return( 
-            Object.keys(this.state).map(key => {
+            Object.keys(this.state.flights).map(flight => {
                 return(
-                    <li> {key} </li>
+                    <ul>
+                        <li> { flight.from } </li>
+                        <li> { flight.to } </li>
+                        <li> { flight.date }</li>
+                    </ul>
                 )
             })
         )
@@ -36,24 +49,30 @@ class SearchBar extends Component {
     
         _handleSearch = (e) => {
             e.preventDefault();
-            this.props.SearchFlights(this.state.origin, this.state.destination)
+            this.props.SearchFlights()
         };
 
     render(){
         return (
             <div>
-                <div classs="search-entry-fields">
-                    <input onChange={ this._handleOrigin } placeholder="From" />
-                    <input onChange={ this._handleDestination } placeholder="To" /> 
-                </div>
-                <div>
-                    <a onClick={ this._handleSearch } >Search Flights</a>
-                </div>
+                <form onSubmit={ this._handleSearch }>
+                        <input onChange={ this._handleOrigin } placeholder="From" />
+                        <input onChange={ this._handleDestination } placeholder="To" /> 
+                    <button>Search Flights</button>
+                </form>
+
+                <tr> 
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
             </div>
             
         )
     };
 }
+
+export default SearchBar 
 
 
 
